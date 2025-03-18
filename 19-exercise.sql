89bb545136b678972de77cbae906ef80f9c6cee3
Gopall Borse <gopallborse@gmail.com>
2025-03-18 22:24:38 +0530

    exercise

---
+++ 19-exercise.sql
@@ -0,0 +1,61 @@
+-- Task 1: Create Database
+-- CREATE DATABASE online_shop;
+
+-- Tasks 2 + 3: Create and Configure table
+-- CREATE TABLE products (
+--     name VARCHAR(200),
+--     price NUMERIC(10, 2), -- 8.99, 12345.99
+--     description TEXT,
+--     amount_in_stock SMALLINT,
+--     image_path VARCHAR(500) -- 'uploads/images/products/some-product.jpg'
+-- );
+
+-- Task 4: Inserting dummy data
+-- INSERT INTO products (
+--         name,
+--         price,
+--         description,
+--         amount_in_stock,
+--         image_path
+--     )
+-- VALUES (
+--         'Product 1',
+--         10.99,
+--         'This is the first product',
+--         100,
+--         'uploads/images/products/product-1.jpg'
+--     ),
+--     (
+--         'Product 2',
+--         20.99,
+--         'This is the second product',
+--         200,
+--         'uploads/images/products/product-2.jpg'
+--     ),
+--     (
+--         'Product 3',
+--         30.99,
+--         'This is the third product',
+--         300,
+--         'uploads/images/products/product-3.jpg'
+--     );
+
+-- Task 5: Adding constraints
+-- ALTER TABLE products
+-- MySQL
+-- MODIFY COLUMN name VARCHAR(200) NOT NULL,
+-- MODIFY COLUMN price NUMERIC(10, 2) NOT NULL CHECK (price > 0),
+-- MODIFY COLUMN description TEXT NOT NULL,
+-- MODIFY COLUMN amount_in_stock SMALLINT CHECK (amount_in_stock >= 0);
+
+-- PostgreSQL
+-- ALTER COLUMN name SET NOT NULL,
+-- ALTER COLUMN price SET NOT NULL,
+-- ALTER COLUMN description SET NOT NULL,
+-- ADD CONSTRAINT price_positive CHECK (price > 0),
+-- ADD CONSTRAINT amount_in_stock_positive CHECK (amount_in_stock >= 0);
+
+-- Task 6: Adding id column
+-- ALTER TABLE products
+-- ADD COLUMN id INT PRIMARY KEY AUTO_INCREMENT; -- MySQL
+-- ADD COLUMN id SERIAL PRIMARY KEY; -- PostgreSQL
