4e52e7b2a17d01afe96ba25b71a027b616eb4914
Gopall Borse <gopallborse@gmail.com>
2025-03-20 15:52:54 +0530

    left join

---
+++ 03-data_normalization_and_relationships/06-left-join.sql
@@ -0,0 +1,8 @@
+-- SELECT *
+-- FROM users AS u
+-- LEFT JOIN addresses AS a ON a.id = u.address_id;
+
+SELECT *
+FROM addresses AS a
+    LEFT JOIN users AS u ON a.id = u.address_id
+    LEFT JOIN cities AS c ON a.city_id = c.id;
