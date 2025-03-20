6bd3cfd519ba76081ec52a9d00ebd18e9d5adbd4
Gopall Borse <gopallborse@gmail.com>
2025-03-20 15:32:11 +0530

    combining multiple inner joins

--- 03-data_normalization_and_relationships/04-inner-join.sql
+++ 03-data_normalization_and_relationships/04-inner-join.sql
@@ -8,6 +8,7 @@ SELECT u.id AS user_id,
     email,
     street,
     house_number,
-    city_id
+    c.name AS city_name
 FROM users AS u
-    INNER JOIN addresses AS a ON u.address_id = a.id;
+    INNER JOIN addresses AS a ON u.address_id = a.id
+    INNER JOIN cities AS c ON a.city_id = c.id;
